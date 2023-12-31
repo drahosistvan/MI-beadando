95f5a1a949939f36bc5a800cc33a8507adc13d05
Istvan Drahos <drahos.istvan@gmail.com>
2023-12-31 13:55:35 +0100

    main program refactor

--- MestersegesIntelligenciaAlapjaiBeadando/Program.cs
+++ MestersegesIntelligenciaAlapjaiBeadando/Program.cs
@@ -6,57 +6,87 @@ namespace MestersegesIntelligenciaAlapjaiBeadando
     {
         static void Main(string[] args)
         {
-            char solution;
             bool inputIsValid;
+            string[] validInputs;
+            string pickedSolution;
+
+            List<Solution> solutions = solutions = new List<Solution> { new FruitExchangeSolution(), new PlayerStepperSolution() }; ;
 
             Console.WriteLine(
                     "Készítő: Drahos István (S94VGD) \n " +
                     "Milton Friedman Egyetem\n " +
                     "2023\n\n");
 
-            Console.WriteLine(
-                    "Az alábbi program két állapottér reprezentációs feladat futtatását valósítja meg. \n " +
-                    "A.) 2.5-ös feladat\n " +
-                    "B.) 2.43-as feladat " +
-                    "\n\n Kérem adja meg, hogy melyiket szeretné futtatni (A vagy B)");
+            Console.WriteLine("A program az alábbi állapottér reprezentációs feladatok futtatását valósítja meg.");
+
+            validInputs = new string[solutions.Count];
+
+            int index = 0;
+            foreach (var solution in solutions)
+            {
+                Console.WriteLine("\n\n" + solution.GetNumber());
+                Console.WriteLine(solution.GetDescription());
+
+                validInputs[index] = solution.GetNumber();
+                index++;
+            }
+
+            Console.Write("\n\nKérem adja meg a futtatni kívánt feladat számát: ");
+
             do
             {
                 string output = Console.ReadLine();
 
-                inputIsValid = !string.IsNullOrEmpty(output) && output.Length == 1;
-                solution = inputIsValid ? output[0] : ' ';
+                inputIsValid = !string.IsNullOrEmpty(output) && validInputs.Contains(output);
 
-                if (inputIsValid && (solution == 'A' || solution == 'B'))
+                if (!inputIsValid)
                 {
-                    inputIsValid = true;
+                    Console.WriteLine("\nKérem, hogy megfelelő feladatszámot adjon meg!");
+                    Console.Write("Kérem adja meg a futtatni kívánt feladat számát: ");
                 }
-                else
-                {
-                    Console.WriteLine("Hibás bemenet! Kérem adjon meg A-t vagy B-t.");
-                    inputIsValid = false;
-                }
-
+                
+                pickedSolution = output;
 
             } while (!inputIsValid);
 
-            if (solution.ToString() == "A")
+            foreach (var solution in solutions)
             {
-                Solution1();
-            } else {
-                Solution2();
+                if (solution.GetNumber() == pickedSolution)
+                {
+                    solution.Solve();
+                    break;
+                }
             }
         }
+    }
 
-        static void Solution1()
+    abstract class Solution
+    {
+        protected string number;
+        protected string description;
+
+        public abstract void Solve();
+        public abstract string GetNumber();
+        public abstract string GetDescription();
+    }
+
+    class FruitExchangeSolution : Solution
+    {
+        public FruitExchangeSolution()
+        {
+            this.number = "2.5";
+            this.description = "Van 13 almánk, 46 körténk és 59 darab barackunk. Egy-egy különböző gyümölcsért cserébe két darabot kapunk a harmadik fajtából a csősztől. Ügyesen csere-berélve érjük el, hogy csak egyetlen fajta gyümölcsünk maradjon!";
+        }
+        public override void Solve()
         {
             AbsztraktÁllapot k = new FruitExchange(13, 46, 59);
             Csúcs S = new Csúcs(k);
             KeresőfávalKereső kereső = new MélységiKeresés();
             Csúcs T = kereső.Keres(S);
-            if (T == null) { Console.WriteLine("No solution was found!"); }
+            if (T == null) { Console.WriteLine("Nincs megoldás!"); }
             else
             {
-                Console.WriteLine("Got the solution!");
+                Console.WriteLine("Megvan a megoldás");
                 Csúcs Temp = T;
                 while (Temp != null)
                 {
@@ -67,7 +97,25 @@ namespace MestersegesIntelligenciaAlapjaiBeadando
             Console.ReadLine();
         }
 
-        static void Solution2()
+        public override string GetNumber()
+        {
+            return this.number;
+        }
+
+        public override string GetDescription()
+        {
+            return this.description;
+        }
+    }
+
+    class PlayerStepperSolution : Solution
+    {
+        public PlayerStepperSolution()
+        {
+            this.number = "2.43";
+            this.description = "A feladat leírásában látható képen látható tábla S jelű mezőjére helyezett figurával a C jelű mezőre kell eljutni. A figurát minden lépésben függőlegesen vagy vízszintesen lehet elmozdítani egy mezővel. A fehér mezőkről előre kell lépni, azaz abba az irányba amelybe a figura a megelőző lépésben lépett. Az előző lépés iránya a figura haladási iránya. A piros mezőkről a figurával előre lehet lépni, vagy pedig a haladási irányhoz képes jobbra fordulva, a kék mezőkről előre, vagy pedig balra fordulva. Az első lépést megelőzően a figuráa haladási iránya észak, tehát az induló mezőről a figurával abba az irányba kell lépni.";
+        }
+        public override void Solve()
         {
             List<String> blueCells = ["0,0", "1,0", "1,2", "1,5", "2,2", "3,0", "3,1", "3,2", "3,4", "5,2"];
             List<String> redCells = ["0,4", "2,1", "2,3", "4,0", "4,1", "4,3", "4,4", "5,3", "5,6", "6,2", "6,3"];
@@ -89,8 +137,17 @@ namespace MestersegesIntelligenciaAlapjaiBeadando
             }
             Console.ReadLine();
         }
-    }
 
+        public override string GetNumber()
+        {
+            return this.number;
+        }
+
+        public override string GetDescription()
+        {
+            return this.description;
+        }
+    }
 }
 
 
